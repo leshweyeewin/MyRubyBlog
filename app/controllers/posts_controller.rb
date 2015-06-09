8ec69b7d2a60c4cacd9cb24bbbd0104de2d6871f
leshweyeewin <www.pancherry.com@gmail.com>
2015-06-10 01:25:22 +0800

    heroku update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
 	def index
-		@content_first = 'This is some sample text for our awesome new Ruby blog';
-		@content_second = 'This is more sample text for our awesome new Ruby blog';
+		@posts = Post.all
 	end
 
 	def new
