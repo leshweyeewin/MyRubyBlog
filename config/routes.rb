2cc61dfc02e936ce8ed77a621f2653a16b2f802d
leshweyeewin <www.pancherry.com@gmail.com>
2015-06-11 08:12:40 +0800

    static pages and navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'pages/resources'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :categories
@@ -14,6 +20,9 @@ Rails.application.routes.draw do
   get 'home/index'
     resources :posts
     resources :categories
+    get '/about' , :to => 'pages#about'
+    get '/contact' , :to => 'pages#contact'
+    get '/resources' , :to => 'pages#resources'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
